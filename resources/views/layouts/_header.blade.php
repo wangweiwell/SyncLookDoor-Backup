fd8efd2eebf5cc181a7dc18629a317e08f54988d
wangweiwell <wangwei4611@qq.com>
2018-02-18 02:22:51 +0800

    Finish layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,16 @@
+<header>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-primary">
+  <!-- Brand -->
+  <a id="logo" class="navbar-brand" href="{{ route('home') }}">SyncLookDoor</a>
+  <!-- Links -->
+  <div class="collapse navbar-collapse justify-content-end" id="nav-content">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('help') }}">帮助</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">登录</a>
+    </li>
+  </ul>
+  </nav>
+</header>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','Sample') - My first laravel project!</title>
+    <title>@yield('title','SyncLookDoor') - My first laravel project!</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
+  @include('layouts._header')
+
+  <div class="container">
     @yield('content')
+    @include('layouts._footer')
+  </div>
   </body>
 </html>
