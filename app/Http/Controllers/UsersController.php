f170b7ac3487b28b1decde8e3e353457de137f40
wangweiwell <wangwei4611@qq.com>
2018-02-19 17:17:33 +0800

    Finish user edit, update, index, and destroy actions.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,34 +8,81 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+        'except' => ['show', 'create', 'store', 'index']
+    ]);
+    }
+
     public function create()
     {
-      return view('users.create');
+        return view('users.create');
     }
 
     public function show(User $user)
     {
-      return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     public function store(Request $request)
     {
-      $this->validate($request, [
+        $this->validate($request, [
         'name' => 'required|max:50',
         'email' => 'required|email|unique:users|max:255',
         'password' => 'required|confirmed|min:6'
       ]);
 
-      $user = User::create([
+        $user = User::create([
         'name' => $request->name,
         'email' => $request->email,
         'password' => bcrypt($request->password),
       ]);
 
-      Auth::login($user); //注册后自动登录
+        Auth::login($user); //注册后自动登录
 
-      session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
-      return redirect()->route('users.show',[$user]);
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+        'name' => 'required|max:50',
+        'password' => 'nullable|confirmed|min:6'
+    ]);
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -6,14 +6,14 @@
   <div class="collapse navbar-collapse justify-content-end" id="nav-content">
   <ul class="navbar-nav" style="margin-right:100px;">
     @if (Auth::check())
-     <li class="nav-item"><a class="nav-link" href="#">用户列表 </a></li>
+     <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">用户列表 </a></li>
      <li class="dropdown  class="nav-item"">
        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
          {{ Auth::user()->name }} <b class="caret"></b>
        </a>
        <ul class="dropdown-menu">
          <li  class="nav-item"><a class="nav-link" href="{{ route('users.show', Auth::user()->id) }}">个人中心</a></li>
-         <li  class="nav-item"><a class="nav-link" href="#">编辑资料</a></li>
+         <li  class="nav-item"><a class="nav-link" href="{{ route('users.edit', Auth::user()->id) }}">编辑资料</a></li>
          <li class="divider  class="nav-item""></li>
          <li  class="nav-item">
            <a id="logout" class="nav-link" href="#">
